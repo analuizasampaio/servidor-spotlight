07709a09cc8f658bbf938995cc046c0835b6a07f
Ana Luiza Sampaio <sampaioaanaluiza@gmail.com>
2019-06-18 22:23:31 -0300

    adicionar schema

---
+++ hospitaisSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const hospitaisSchema = new Schema({
+    _id: { type: mongoose.Schema.Types.ObjectId, auto: true},
+    nomeHospital: {type: String, required: true},
+    nomeUser: {type: String},
+    image: {type: String},
+    nota: {type: Number, required: true},
+    relato: {type: String, required: true}
+})
+
+const hospitaisModel = mongoose.model("hospitais", hospitaisSchema)
+module.export = hospitaisModel
