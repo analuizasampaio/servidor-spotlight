aec46afa5ceb4a1aecdeb8d7ba13312397bb82b4
Ana Luiza Sampaio <sampaioaanaluiza@gmail.com>
2019-06-18 21:56:15 -0300

    Adicionar get ao server

--- server.js
+++ server.js
@@ -8,3 +8,14 @@ const PORT = 3000
 servidor.use(cors())
 servidor.use(bodyParser.json())
 
+servidor.get('/',(request, response)=>{
+    response.send('Olá, mundo!')
+})
+
+servidor.get('/hospitais', async (request, response)=>{
+    controler.getAll()
+    .then(hospitais => response.send(hospitais))
+})
+
+servidor.listen(PORT)
+console.info(`Rodando na porta ${PORT}`)
