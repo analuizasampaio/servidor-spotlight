0e95967fd9debb5e06b4cc6b43800c50ba811958
Ana Luiza Sampaio <sampaioaanaluiza@gmail.com>
2019-06-20 13:44:57 -0300

    Adicionar repositoy

---
+++ hospitaisRepository.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const MONGO_URL = "mongodb://localhost:27017/hospitais";
+
+function connect () {
+    mongoose.connect(MONGO_URL,
+      { useNewUrlParser: true },
+      function (error) {
+        if(error) {
+          console.error("Algo de errado não está certo: ", error)
+        } else {
+          console.log("Conectado no mongoDB.")
+        }
+      }
+    );
+  }
+  
+  module.exports = { connect }
