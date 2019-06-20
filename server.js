9abe14dfa7298bcbd47666c71790aa0b5fd8e069
Ana Luiza Sampaio <sampaioaanaluiza@gmail.com>
2019-06-20 13:55:14 -0300

    Adicionar get por id ao server.js

--- server.js
+++ server.js
@@ -17,5 +17,24 @@ servidor.get('/hospitais', async (request, response)=>{
     .then(hospitais => response.send(hospitais))
 })
 
+servidor.get('/hospitais/:id', (request, response)=>{
+    const hospitalId = request.params.hospitalId
+    controler.getById(hospitalId)
+    .then(hospital =>{
+        if(!hospital){
+            response.sendStatus(404)
+        }else{
+            response.send(hospital)
+        }
+    })
+    .catch(error =>{
+        if(error.name === "CastError"){
+            response.sendStatus(400)
+        }else{
+            response.sendStatus(500)
+        }
+    })
+})
+
 servidor.listen(PORT)
 console.info(`Rodando na porta ${PORT}`)
