9e58fa301dc9504c6134da7080720c4e3b577780
Ana Luiza Sampaio <sampaioaanaluiza@gmail.com>
2019-06-20 13:45:24 -0300

    Adicionar controller

---
+++ hospitaisController.js
@@ -0,0 +1,38 @@
+const hospitaisModel = require('./hospitaisSchema')
+
+connect()
+
+const getAll = async () =>{
+    return hospitaisModel.find((error, hospitais)=>{
+        return hospitais
+    })
+}
+
+const getById = (id) =>{
+    return hospitaisModel.findById(id)
+}
+
+const add = (hospital) => {
+    const novoHospital = new hospitaisModel(hospital)
+    return novoHospital.save()
+}
+
+const remove = (id)=>{
+    return hospitaisModel.findByIdAndDelete(id)
+}
+
+const update = (id, hospital) =>{
+    return hospitaisModel.findByIdAndUpdate(
+        id, 
+        { $set: hospital },
+        { new: true },
+    )
+}
+
+module.exports = {
+    getAll,
+    getById,
+    add,
+    remove,
+    update
+}
